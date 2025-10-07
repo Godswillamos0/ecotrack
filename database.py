33549ff7a2b5abc10bc529fa107a59f50d1a058f
Godswill Amos <amosgodswill00@gmail.com>
2025-10-07 08:40:43 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,9 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://faramade:I1298R4emEhtKC5kkJi5qBffxHmkYYUC@dpg-d3ibmummcj7s73942b40-a.oregon-postgres.render.com/faramadeproject'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
+
