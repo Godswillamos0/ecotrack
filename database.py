835c9b559e21b0d8d880b208977e6f063936ce42
Godswill Amos <amosgodswill00@gmail.com>
2025-10-07 08:28:35 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./ecotraca.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://faramade:I1298R4emEhtKC5kkJi5qBffxHmkYYUC@dpg-d3ibmummcj7s73942b40-a.oregon-postgres.render.com/faramadeproject'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
